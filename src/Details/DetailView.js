99ec2d9c593e8051718ca137d7be0a0ed57fd18d
AGoldmintz <agoldmintz@gmail.com>
2020-10-18 12:37:08 -0500

    Clean up icon syntax

--- src/Details/DetailView.js
+++ src/Details/DetailView.js
@@ -13,11 +13,11 @@ const DetailView = ({
     //TODO: Don't display modal until all props are loaded
     <div className="details-wrapper">
       <div className="details-controls">
-        {id !== 1 && <i class="fas fa-arrow-circle-left" onClick={getPrev} />}
+        {id !== 1 && <i className="fas fa-arrow-circle-left" onClick={getPrev} />}
         {id !== resultsLength && (
-          <i class="fas fa-arrow-circle-right" onClick={getNext} />
+          <i className="fas fa-arrow-circle-right" onClick={getNext} />
         )}
-        <i class="fas fa-times-circle" onClick={toggleDetailModal} />
+        <i className="fas fa-times-circle" onClick={toggleDetailModal} />
       </div>
       <div className="details-content">
         <div>NAME: {name[`name-${lang}`]}</div>
